37ff0c0c503e1027a8dbb24b7b011daa9794fa36
Daria-Korotych <dkorotych@kse.org.ua>
2026-02-03 17:55:38 +0100

    feat: add types.ts for movies and sessions

---
+++ src/features/movies/types.ts
@@ -0,0 +1,33 @@
+export interface Movie {
+    id: number;
+    title: string;
+    originalTitle: string;
+    rating: number;
+    genre: string[];
+    ageRestriction: string;
+    year: number;
+    duration: number;
+    endDate: string;
+    description: string;
+    actors: string[];
+    poster: string;
+    formats: SessionFormat[];
+}
+
+export type SessionFormat = "2D" | "3D" | "IMAX" | "4DX";
+
+export interface Session {
+    id: number;
+    movieId: number;
+    time: string;
+    format: SessionFormat;
+    hall: number;
+    price: number;
+    availableSeats: number;
+}
+
+export interface DateOption {
+    value: string;
+    day: string;
+    date: number;
+}
