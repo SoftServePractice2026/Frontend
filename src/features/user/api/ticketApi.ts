a0948d6fbe9d3bc8ea0b761ac55b4424310a053c
QusDev <qusdeveloper@gmail.com>
2026-02-10 21:03:42 +0200

    rewrite ticketApi methods

--- src/features/user/api/ticketApi.ts
+++ src/features/user/api/ticketApi.ts
@@ -1,7 +1,7 @@
 import type {TicketStatus} from "@/features/user/types/TicketVm.ts";
+import { api } from "@/shared/api/Axios";
 
 export async function getMyTickets(status: TicketStatus, userId: string) {
-    const res = await fetch(`/api/v1/tickets?TicketStatus=${status}&UserId=${userId}`);
-    if (!res.ok) throw new Error("Failed to load");
-    return res.json();
+    const response = await api.get(`v1/tickets?TicketStatus=${status}&UserId=${userId}`);
+    response.data;
 }
