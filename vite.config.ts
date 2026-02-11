fe8ae0080e1d8f7d12c06c40ce8baf928ac87bbd
QusDev <qusdeveloper@gmail.com>
2026-02-11 19:22:33 +0200

    add env to project

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,29 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  const apiUrl = env.VITE_API_URL
+
+  return {
+    plugins: [
+      react(),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
     },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://localhost:7087",
-        changeOrigin: true,
-        secure: false,
+    server: {
+      proxy: {
+        "/api": {
+          target: apiUrl,
+          changeOrigin: true,
+          secure: false,
+        }
       }
     }
   }
